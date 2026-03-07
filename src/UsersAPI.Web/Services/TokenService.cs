1719e890d697ac8ff858b637a209c7b0c60414bf
Joao Paulo de Moraes da Costa <joao.moraes@basisti.com.br>
2026-03-07 17:45:11 -0400

    feat: Implementação do TokenService

---
+++ src/UsersAPI.Web/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsersAPI.Domain;
+
+namespace UsersAPI.Web.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+        public TokenService(IConfiguration config) => _config = config;
+
+        public string GenerateToken(User user, IList<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(_config["JwtExpirationsInMinutes"]!)),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+    }
+}
